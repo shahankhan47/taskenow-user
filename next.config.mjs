540e00fa559e79b602ab24fc89ebf32bfc155471
Shahan Khan <prince47khan@gmail.com>
2024-04-04 19:40:38 +0530

    changes

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'utfs.io',
+          },
+        ],
+        remotePatterns:[
+          {
+            protocol:"https",
+            hostname:'images.pexels.com'
+          }
+        ]
+      },
+};
+
+export default nextConfig;
